cfe987e479706b53a33f2065ec3130e40c33619d
Kazim Mirza <kazim@techultra.in>
2020-08-05 12:04:01 +0530

    [ADD] : Added modules

---
+++ tus_pricelist_price/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Pricelist price ',
+    'version': '13.0.22',
+    'license': 'OPL-1',
+    'category': 'Sale',
+    'summary': 'This app use for add Pricelist price in view ',
+
+    'author': 'TechUltra Solutions',
+    'website': 'https://www.techultrasolutions.com/',
+    'maintainer': 'TechUltra Solutions',
+    'depends': ['sale_management'],
+    'data': ['views/product_template_view.xml',
+             'views/product_product_view.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'images': ['static/description/icon.png'],
+    'active': False,
+    'price': 00.00,
+    'currency': 'EUR',
+}
