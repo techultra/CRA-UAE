6c79bed000a99d3e1f36baff12c3b0017b4cce43
Kazim Mirza <kazim@techultra.in>
2020-10-26 11:22:35 +0530

    [ADD] : Added module

--- tus_cra_extended/models/hr_contract.py
+++ tus_cra_extended/models/hr_contract.py
@@ -4,6 +4,6 @@ from odoo import fields, models, _
 class Contract(models.Model):
     _inherit = 'hr.contract'
 
-    variable = fields.Monetary('Variable', required=True, tracking=True)
+    variable = fields.Monetary('Variable', tracking=True)
     limit = fields.Selection([('limited', 'Limited'),
                               ('unlimited', 'Unlimited')], string="Limit")
