0fc22e39d646d7d9e7475a0e15320899a88b1223
Megha <megha.techultra@gmail.com>
2021-01-07 18:29:35 +0530

    [MOD]Modify the gratuity report

---
+++ tus_cra_extended/reports/hr_leave_type.py
@@ -0,0 +1,9 @@
+from openerp import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from datetime import datetime
+
+
+class HolidaysTypes(models.Model):
+    _inherit = 'hr.leave.type'
+
+    is_unpaid = fields.Boolean(string="Is Unpaid")
